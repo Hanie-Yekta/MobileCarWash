a7c15e1f423ce7692c7e2df75ac9d4920b1229b1
Parnaz Alagheband <parnazalagheband@gmail.com>
2025-01-11 21:37:28 +0330

    chore: add icons

---
+++ CarWashFrontEnd/car-wash/src/assets/icons/login.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg
+    width="64px"
+    height="64px"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+    <g
+      id="SVGRepo_tracerCarrier"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></g>
+    <g id="SVGRepo_iconCarrier">
+      <path
+        d="M14 4L17.5 4C20.5577 4 20.5 8 20.5 12C20.5 16 20.5577 20 17.5 20H14M15 12L3 12M15 12L11 16M15 12L11 8"
+        stroke="#9c9c9c"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></path>
+    </g>
+  </svg>
+</template>
