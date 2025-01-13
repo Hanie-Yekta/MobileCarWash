aad0dfc181aa7ca046155eb0dba9842882315a08
Parnaz Alagheband <parnazalagheband@gmail.com>
2025-01-13 18:08:30 +0330

    chore: initial commit

---
+++ CarWashFrontEnd/car-wash/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import Navbar from "@/components/common/main-navbar.vue";
+import { useRoute } from 'vue-router';
+import { computed} from 'vue';
+
+const route = useRoute();
+
+const showNavbar = computed(() => !route.meta.isAuthenticate);
+
+</script>
+
+<template>
+  <navbar  v-show="showNavbar" />
+  <RouterView />
+</template>
