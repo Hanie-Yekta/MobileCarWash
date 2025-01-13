d852b2e925be6f497edf40c7e462ac2ff77a86c2
Parnaz Alagheband <parnazalagheband@gmail.com>
2025-01-13 18:34:36 +0330

    feat: implement blank dashboard component

---
+++ CarWashFrontEnd/car-wash/src/components/view/blank-dashboard.vue
@@ -0,0 +1,26 @@
+<script setup>
+import BaseIcon from "@/components/common/base-icon.vue";
+</script>
+
+<template>
+  <div class="blank-dashboard">
+    <div class="blank-dashboard__text" >سفارشی وجود ندارد.</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.blank-dashboard {
+  height: 400px;
+  border-radius: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  align-items: center;
+  justify-content: center;
+
+  &__text{
+    color:rgb(183, 183, 183);
+    font-size:20px;
+  }
+}
+</style>
